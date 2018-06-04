dc245a476b47598df78af1f4ca53536b0ca61562
Justin Santa Barbara <justin@fathomdb.com>
2018-06-03 21:53:09 -0700

    Fix some of the problems with API doc generation
    Some missing dependencies in apiserver-builder

--- hack/make-gendocs.sh
+++ hack/make-gendocs.sh
@@ -28,12 +28,17 @@ trap cleanup EXIT
 mkdir -p ${WORK_DIR}/go/
 ln -s ${GOPATH}/src/k8s.io/kops/vendor/ ${WORK_DIR}/go/src
 
+GOPATH=${WORK_DIR}/go/ go install github.com/kubernetes-incubator/reference-docs/gen-apidocs
+cp ${WORK_DIR}/go/bin/gen-apidocs ${GOPATH}/bin/
+
 GOPATH=${WORK_DIR}/go/ go install k8s.io/code-generator/cmd/openapi-gen
 cp ${WORK_DIR}/go/bin/openapi-gen ${GOPATH}/bin/
 
 # Install the apiserver-builder commands
 GOPATH=${WORK_DIR}/go/ go install github.com/kubernetes-incubator/apiserver-builder/cmd/...
 cp ${WORK_DIR}/go/bin/openapi-gen ${GOPATH}/bin/
+cp ${WORK_DIR}/go/bin/apiserver-boot ${GOPATH}/bin/
+cp ${WORK_DIR}/go/bin/apiregister-gen ${GOPATH}/bin/
 
 # Install the reference docs commands (apiserver-builder commands invoke these)
 GOPATH=${WORK_DIR}/go/ go install github.com/kubernetes-incubator/reference-docs/gen-apidocs/...
